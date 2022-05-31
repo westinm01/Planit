4330c76915c9dc1d544aadb0b157b46fd4de1bed
Wes <wmont012@ucr.edu>
2022-05-31 00:51:20 -0700

    Added a navigation bar

--- planit/src/App.js
+++ planit/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
+      
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ planit/src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+
+const NavBar = () => {
+    
+    return (
+      <Navbar bg="light" expand="lg">
+        <Container>
+          
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/home">Home</Nav.Link>
+              <Nav.Link href="/makeAPlanit">Make a Planit</Nav.Link>
+              <Nav.Link href="/profile">Profile</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          <Navbar.Collapse className="justify-content-end" id="navbar-right">
+              <Nav>
+                <Nav.Link href="/login">Login</Nav.Link>
+                <Nav.Link href="/register">Sign Up</Nav.Link>
+              </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  };
+
+  export default NavBar;

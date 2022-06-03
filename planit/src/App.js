c9d270d8ffd6efc1640049d98acd1e41c80a0d72
nathanttran <t.tran.nathan@gmail.com>
2022-06-03 00:24:31 -0700

    Started making home page

--- planit/src/App.js
+++ planit/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { Route, Routes } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
+import Home from "./components/Home"
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Home />
       <header className="App-header">
       
         <img src={logo} className="App-logo" alt="logo" />
---
+++ planit/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () =>{
+    return(
+        <div className = "home">
+        <h3 className="homepage"> PlanIt</h3>
+        <p> Put planit logo here</p>
+        <p className = "slogan"> Quick Easy Planning</p>
+        </div>
+    );
+}

4dfcb1e4014973a7ce817e5f71f9d7eb792955f5
nathanttran <t.tran.nathan@gmail.com>
2022-06-08 20:25:47 -0700

    made style changes to home page and added logo

--- planit/src/components/Home.js
+++ planit/src/components/Home.js
@@ -1,11 +1,23 @@
 import React from "react";
+import NavBar from "./NavBar";
+import logo from '../PlanItLogo.png';
 
 const Home = () =>{
     return(
+        <div>
+            <NavBar />
+            <br />
         <div className = "home">
-        <h3 className="homepage"> PlanIt</h3>
-        <p> Put planit logo here</p>
-        <p className = "slogan"> Quick Easy Planning</p>
+            <h3 className="planit-name"> Plan-It</h3>
+            <img src={logo} className="planit-logo" alt="logo" />
+        <p className = "slogan"> Quick and Easy Planning</p>
+
+        <div className= "sidebar-left">
+        <div className= "sidebar-right">
+
+        </div>
+        </div>
+        </div>
         </div>
     );
 }
